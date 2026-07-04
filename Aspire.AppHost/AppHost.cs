17f04bb230d956e486aa9e5856f27e1fc27fba8f
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2026-07-04 20:01:50 +0700

    update to .net 10

--- Aspire.AppHost/AppHost.cs
+++ Aspire.AppHost/AppHost.cs
@@ -6,14 +6,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var webControllerApi = builder.AddProject<Projects.WebControllerApi>("web-controller-api");
 var webMinimalApi = builder.AddProject<Projects.WebMinimalApiAOT>("web-minimap-api");
-var pgsql = builder.AddContainer("postgres", "postgres:latest")
-    .WithContainerName("binh-dev-pgsql-aspire")
-    .WithEnvironment("POSTGRES_PASSWORD", "170116Abc")
-    .WithEnvironment("POSTGRES_USER", "admin")
-    .WithEndpoint(5432, 5432);
+//var pgsql = builder.AddContainer("postgres", "postgres:latest")
+//    .WithContainerName("binh-dev-pgsql-aspire")
+//    .WithEnvironment("POSTGRES_PASSWORD", "170116Abc")
+//    .WithEnvironment("POSTGRES_USER", "admin")
+//    .WithEndpoint(5432, 5432);
 
 
-webControllerApi.WithReference(pgsql);
+//webControllerApi.WithReference(pgsql);
 
 
 webControllerApi.PublishAsDockerFile();
