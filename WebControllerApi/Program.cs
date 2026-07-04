6bb1c226146c11d220f7554baae4ebeb1454600a
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2026-07-04 22:01:22 +0700

    intergrate web api with database

--- WebControllerApi/Program.cs
+++ WebControllerApi/Program.cs
@@ -10,11 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<TestContext>(options =>
-//    options.UseNpgsql(
-//        builder.Configuration.GetConnectionString("Default"),
-//        npgsqlOptions => npgsqlOptions.MigrationsAssembly("MyApi.Infrastructure")
-//));
+builder.Services.AddDbContext<TestContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("Database1")
+));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
